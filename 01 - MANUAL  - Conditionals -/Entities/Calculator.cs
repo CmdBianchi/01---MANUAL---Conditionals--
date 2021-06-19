067b07029470711ca17eb2156f8aff63ad81528c
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-19 10:13:27 -0300

    Commit 03 List 01 Exercise 03

--- 01 - MANUAL  - Conditionals -/Entities/Calculator.cs
+++ 01 - MANUAL  - Conditionals -/Entities/Calculator.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Text;
 namespace Entities {
     class Calculator {
-        public int Number { get; set; }
-        public Calculator(int number) {
-            Number = number;
+        public int NumberX { get; set; }
+        public int NumberY { get; set; }
+        public Calculator(int numberX, int numberY) {
+            NumberX = numberX;
+            NumberY = numberY;
         }
         public bool Verif() {
-            if(Number % 2 == 0) {
+            if(NumberX % NumberY == 0 || NumberY % NumberX == 0) {
                 return true;
             }
             else {
@@ -17,10 +19,10 @@ namespace Entities {
         }
         public override string ToString() {
             if (Verif() == true) {
-                return "Par";
+                return "É Multiplo";
             }
             else {
-                return "Impar";
+                return "Não é Multiplo";
             }
         }
 
--- 01 - MANUAL  - Conditionals -/Program.cs
+++ 01 - MANUAL  - Conditionals -/Program.cs
@@ -3,9 +3,10 @@ using Entities;
 namespace _01___MANUAL____Conditionals__ {
     class Program {
         static void Main(string[] args) {
-            Console.Write("Digite o valor do numero p/ informar se é PAR ou IMPAR: "); int x = int.Parse(Console.ReadLine());
+            Console.Write("Digite o valor X do numero p/ informar se é multiplo: "); int x = int.Parse(Console.ReadLine());
+            Console.Write("\nDigite o valor Y do numero p/ informar se é multiplo: "); int y = int.Parse(Console.ReadLine());
             Console.WriteLine("");         
-            Calculator calculator = new Calculator(x);
+            Calculator calculator = new Calculator(x, y);
             Console.WriteLine(calculator);
         }
     }
